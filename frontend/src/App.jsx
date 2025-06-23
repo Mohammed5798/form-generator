b633e62952ba55d09492f8ce24dc0f77b7cff41a
Miss Teba <127965026+teb7a7@users.noreply.github.com>
2025-06-23 12:35:29 +0300

    Add Login And Sign Up Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
-import './style/App.css';
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 function App() {
-
+  
   return (
-    <>
-    <h1>hello</h1>
-    </>
+   <div className=''>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+   </div>
   )
 }
 
-export default App;
+export default App
