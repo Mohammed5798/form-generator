ecf95397718114d622b79a88ed7847d06903cdb7
hussein3od <hhssing0@gmail.com>
2025-06-24 14:06:46 +0300

    Main Page Responsive

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import { CgTemplate } from "react-icons/cg";
+import { FaBrain } from "react-icons/fa6";
+import { MdDashboardCustomize } from "react-icons/md";
+import { IoDocumentsSharp } from "react-icons/io5";
+import React, { useState, useRef } from "react";
 import Sidebar from "./Sidebar";
 
 const Header = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const toggleSidebar = () => {
     setIsSidebarOpen((prev) => !prev);
@@ -12,28 +17,51 @@ const Header = () => {
     setIsSidebarOpen(false);
   };
 
+  function translate_list() {
+    if (menuRef.current.classList.contains('open')) {
+      menuRef.current.style.transform = "translateX(100%)";
+      menuRef.current.classList.remove("open")
+    }else{
+      menuRef.current.style.transform = "translateX(-100%)";
+      menuRef.current.classList.add("open")
+    }
+  }
+
   return (
     <>
-        <nav className="bg-[#7050EF] flex justify-between items-center p-4 text-white">
-            <div className="flex items-center space-x-3 ml-6">
-                <img src="/logo.png" alt="user" className="w-16" />
-                <span className="font-bold text-lg">Infinity</span>
-            </div>
+      <nav className="bg-[#7050EF] flex justify-between items-center p-4 text-white">
+        <div className="flex items-center space-x-3 ml-6">
+          <img src="/logo.png" alt="user" className="w-16" />
+          <span className="font-bold text-lg">Infinity</span>
+        </div>
 
-            <div className="flex space-x-6">
-                <a href="#" className="hover:underline">AI Form</a>
-                <a href="#" className="hover:underline">My Forms</a>
-                <a href="#" className="hover:underline">Customize</a>
-                <a href="#" className="hover:underline">Templates</a>
-            </div>
+        <div className="list flex space-x-6">
+          <a href="#" className="hover:underline">AI Form</a>
+          <a href="#" className="hover:underline">My Forms</a>
+          <a href="#" className="hover:underline">Customize</a>
+          <a href="#" className="hover:underline">Templates</a>
+        </div>
 
-            <div className="bg-gray-200 rounded-full w-10 h-10 cursor-pointer mr-6 flex items-center justify-center" onClick={toggleSidebar}>
-                <span className="text-2xl text-gray-600">👤</span>
-            </div>
-        </nav>
+        <div
+          className="profile bg-gray-200 rounded-full w-10 h-10 cursor-pointer mr-6 flex items-center justify-center"
+          onClick={toggleSidebar}
+        >
+          <span className="text-2xl text-gray-600">👤</span>
+        </div>
+        <div onClick={translate_list} className="hidden burger cursor-pointer">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>
+      <div ref={menuRef} className="mobile-menu transition-all duration-300 ease-in-out rounded-bl-[10px]">
+        <a href="#" className="hover:bg-[#c4c4c4] block"><FaBrain className="inline mr-[10px]" />AI Form</a>
+        <a href="#" className="hover:bg-[#c4c4c4] block"><IoDocumentsSharp className="inline mr-[10px]" />My Forms</a>
+        <a href="#" className="hover:bg-[#c4c4c4] block"><MdDashboardCustomize className="inline mr-[10px]" />Customize</a>
+        <a href="#" className="hover:bg-[#c4c4c4] block"><CgTemplate className="inline mr-[10px]" />Templates</a>
+      </div>
 
       <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
-      
     </>
   );
 };
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,3 +1,4 @@
+import '../style/App.css';
 import React from "react";
 import Header from "../components/Header";
 import Card from "../components/Card";
